ab552193163d86f75d386d1ca3c19623ff6ecfb5
Khiem Luong <78592170+khiemgluong@users.noreply.github.com>
2024-07-14 23:10:58 -0700

    feat: node curves are now color coded and has dynamic start and end points based on the connection type

--- Assets/Polity/Editor/PolityManagerEditorWindow.cs
+++ Assets/Polity/Editor/PolityManagerEditorWindow.cs
@@ -10,9 +10,27 @@ namespace KhiemLuong
     using static KhiemLuong.PolityManager;
     public class PolityManagerEditorWindow : EditorWindow
     {
+        public enum ConnectionPoint
+        {
+            Top,
+            Right,
+            Left,
+            Bottom
+        }
+        public struct NodeConnection
+        {
+            public int NodeId;
+            public ConnectionPoint Point;
+
+            public NodeConnection(int nodeId, ConnectionPoint point)
+            {
+                NodeId = nodeId;
+                Point = point;
+            }
+        }
         List<Rect> nodes = new List<Rect>();
-        List<int> windowsToAttach = new List<int>();
-        List<int> attachedWindows = new List<int>();
+        List<NodeConnection> windowsToAttach = new List<NodeConnection>();
+        List<NodeConnection> attachedWindows = new List<NodeConnection>();
         float panX = 0;
         float panY = 0;
         public PolityMember rootPolityMember;
@@ -49,27 +67,18 @@ namespace KhiemLuong
             {
                 attachedWindows.Add(windowsToAttach[0]);
                 attachedWindows.Add(windowsToAttach[1]);
-                windowsToAttach = new List<int>();
+                windowsToAttach.Clear();
             }
 
             if (attachedWindows.Count >= 2)
             {
                 for (int i = 0; i < attachedWindows.Count; i += 2)
                 {
-                    DrawNodeCurve(nodes[attachedWindows[i]], nodes[attachedWindows[i + 1]]);
+                    DrawNodeCurve(nodes[attachedWindows[i].NodeId], nodes[attachedWindows[i + 1].NodeId], attachedWindows[i].Point, attachedWindows[i + 1].Point);
+
                 }
             }
 
-            // GUI.BeginGroup(new Rect(panX, panY, 100000, 100000));
-            // DrawNodeCurve(window1, window2);      
-            rootPolityMember = EditorGUILayout.ObjectField("", rootPolityMember, typeof(PolityMember), false) as PolityMember;
-            if (EditorGUI.EndChangeCheck())
-            {
-                if (rootPolityMember != null && PrefabUtility.IsPartOfPrefabAsset(rootPolityMember))
-                {
-                    rootPolityMemberSerializedObj = new SerializedObject(rootPolityMember);
-                }
-            }
             GUI.BeginGroup(new Rect(panX, panY, 100000, 100000));
             BeginWindows();
 
@@ -110,11 +119,6 @@ namespace KhiemLuong
                 Repaint();
             }
         }
-
-        void DrawRootNodeWindow()
-        {
-
-        }
         void DrawNodeWindow(int id)
         {
             // Ensure that there is a placeholder for new nodes if the index exceeds the list count
@@ -131,43 +135,73 @@ namespace KhiemLuong
                     // rootPolityMemberSerializedObj = new SerializedObject(rootPolityMember);
                 }
             }
-            // Additional buttons and functionality
             if (id == 0)
             {
-
                 if (GUILayout.Button("Parents"))
                 {
-                    windowsToAttach.Add(id);
+                    windowsToAttach.Add(new NodeConnection(id, ConnectionPoint.Top));
                 }
                 if (GUILayout.Button("Partners"))
                 {
-                    windowsToAttach.Add(id);
+                    windowsToAttach.Add(new NodeConnection(id, ConnectionPoint.Right));
+                }
+                if (GUILayout.Button("Children"))
+                {
+                    windowsToAttach.Add(new NodeConnection(id, ConnectionPoint.Bottom));
                 }
             }
-
-            if (GUILayout.Button("Children"))
+            else
             {
-                windowsToAttach.Add(id);
+                if (GUILayout.Button("Attach"))
+                {
+                    windowsToAttach.Add(new NodeConnection(id, ConnectionPoint.Left));
+                }
             }
-
             GUI.DragWindow();
         }
 
-        void DrawNodeCurve(Rect start, Rect end)
+        void DrawNodeCurve(Rect start, Rect end, ConnectionPoint startConnection, ConnectionPoint endConnection)
         {
-            DrawNodeCurve(start, end, new Vector2(1.0f, 0.5f), new Vector2(1.0f, 0.5f));
+            Vector2 startPercentage = GetPercentageFromConnectionPoint(startConnection);
+            Vector2 endPercentage = GetPercentageFromConnectionPoint(endConnection);
+            Color lineColor = GetStartConnectionLineColor(startConnection);
+            DrawNodeCurve(start, end, startPercentage, endPercentage, lineColor);
         }
 
-        void DrawNodeCurve(Rect start, Rect end, Vector2 vStartPercentage, Vector2 vEndPercentage)
+        Vector2 GetPercentageFromConnectionPoint(ConnectionPoint point)
+        {
+            switch (point)
+            {
+                case ConnectionPoint.Top: return new Vector2(0.5f, 0f);
+                case ConnectionPoint.Right: return new Vector2(1.0f, 0.5f);
+                case ConnectionPoint.Left: return new Vector2(0.0f, 0.5f);
+                case ConnectionPoint.Bottom: return new Vector2(0.5f, 1f);
+                default: return new Vector2(0.5f, 0.5f); // Default to center if unknown for some reason
+            }
+        }
+
+        Color GetStartConnectionLineColor(ConnectionPoint startConnection)
+        {
+            switch (startConnection)
+            {
+                case ConnectionPoint.Top: return Color.blue;
+                case ConnectionPoint.Right: return Color.green;
+                case ConnectionPoint.Left: return Color.green;
+                case ConnectionPoint.Bottom: return Color.red;
+                default: return Color.black; // Default to center if unknown for some reason
+            }
+        }
+
+        void DrawNodeCurve(Rect start, Rect end, Vector2 vStartPercentage, Vector2 vEndPercentage, Color lineColor)
         {
             Vector3 startPos = new Vector3(start.x + start.width * vStartPercentage.x, start.y + start.height * vStartPercentage.y, 0);
             Vector3 endPos = new Vector3(end.x + end.width * vEndPercentage.x, end.y + end.height * vEndPercentage.y, 0);
             Vector3 startTan = startPos + Vector3.right * (-50 + 100 * vStartPercentage.x) + Vector3.up * (-50 + 100 * vStartPercentage.y);
             Vector3 endTan = endPos + Vector3.right * (-50 + 100 * vEndPercentage.x) + Vector3.up * (-50 + 100 * vEndPercentage.y);
-            Color shadowCol = new Color(200, 200, 0, 200.6f);
+            Color shadowCol = new Color(200, 200, 200, 0.4f);
             for (int i = 0; i < 3; i++) // Draw a shadow
                 Handles.DrawBezier(startPos, endPos, startTan, endTan, shadowCol, null, (i + 1) * 5);
-            Handles.DrawBezier(startPos, endPos, startTan, endTan, Color.white, null, 2);
+            Handles.DrawBezier(startPos, endPos, startTan, endTan, lineColor, null, 2);
         }
 
     }
